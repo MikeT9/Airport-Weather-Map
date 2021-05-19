db968df91f4cda047f7f30ee06b422f901f85fae
wagneree193 <wagneree193@gmail.com>
2021-05-19 17:13:19 -0500

    added airport csv file from kaggle

---
+++ Erin Folder/app.py
@@ -0,0 +1,12 @@
+import numpy as np
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+import warnings
+warnings.filterwarnings('ignore')
--- config.py
+++
@@ -1,3 +0,0 @@
-weather_key= "218661d74970b37f6e9ab6d5a6f76ec2"
-places_key= "AIzaSyATCueMy5aQXAh8WbvVvqRVwBjdVD2XubA"
-java_key= "AIzaSyBsk8ZmBxYTJhNUZb3a9oWBVIVc0MGqorw"
