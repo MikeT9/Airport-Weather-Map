9a92cb54deae8af087f5bf7dabcac2d2fbfd383f
FSR <your_email@fpshami@gmail.com>
2021-05-20 17:31:32 -0500

    v2

--- Fatima's Folder/static/js/logic.js
+++ Fatima's Folder/static/js/logic.js
@@ -18,3 +18,99 @@ var myMap = L.map("map", {
 });
 
 streetmap.addTo(myMap);
+
+d3.csv("static/data/airports.csv").then (function(response) {
+    // Create a new marker cluster group
+  var markers ;
+  
+  // Loop through data
+  for (var i = 0; i < response.length; i++) {
+    markers = L.circleMarker([response[i].LATITUDE, response[i].LONGITUDE])
+    markers.bindPopup(response[i].AIRPORT);
+    L.geoJson(response, {
+      // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup(
+          "Magnitude: "
+            + feature.AIRPORT
+            
+        );
+      }
+    }).addTo(myMap);
+    markers.on('mouseover', function(e) { 
+      d3.csv("static/data/airports.csv").then (function(response){
+        console.log(e)
+        var popup = L.popup()
+      .setLatLng(e.latlng) 
+      .setContent(e.layer)
+      .openOn(myMap);
+    });
+      });
+    
+    markers.on('mouseout', function() { markers.closePopup(); 
+    });
+    
+      // Add a new marker to the cluster group and bind a pop-up
+      //markers.addLayer(L.marker([response[i].LATITUDE, response[i].LONGITUDE]),
+       // Binding a pop-up to our marker
+     
+      myMap.addLayer(markers);
+  }
+  // Add our marker cluster layer to the map
+  
+
+
+
+// //draggable: true,
+// //
+// //.addTo(myMap)
+// // Initialize all of the LayerGroups we'll be using
+// var layers = {
+//   COMING_SOON: new L.LayerGroup(),
+//   EMPTY: new L.LayerGroup(),
+//   LOW: new L.LayerGroup(),
+//   NORMAL: new L.LayerGroup(),
+//   OUT_OF_ORDER: new L.LayerGroup()
+// };
+
+// // Create the map with our layers
+// var map = L.map("map-id", {
+//   center: [40.73, -74.0059],
+//   zoom: 12,
+//   layers: [
+//     layers.COMING_SOON,
+//     layers.EMPTY,
+//     layers.LOW,
+//     layers.NORMAL,
+//     layers.OUT_OF_ORDER
+//   ]
+// });
+
+// // Add our 'lightmap' tile layer to the map
+// lightmap.addTo(myMap);
+
+// // Create an overlays object to add to the layer control
+// var overlays = {
+//   "Coming Soon": layers.COMING_SOON,
+//   "Empty Stations": layers.EMPTY,
+//   "Low Stations": layers.LOW,
+//   "Healthy Stations": layers.NORMAL,
+//   "Out of Order": layers.OUT_OF_ORDER
+// };
+
+// // Create a control for our layers, add our overlay layers to it
+// L.control.layers(null, overlays).addTo(myMap);
+
+// // Create a legend to display information about our map
+// var info = L.control({
+//   position: "bottomright"
+// });
+
+// // When the layer control is added, insert a div with the class of "legend"
+// info.onAdd = function() {
+//   var div = L.DomUtil.create("div", "legend");
+//   return div;
+// };
+// // Add the info legend to the map
+// info.addTo(myMap)});
+});
